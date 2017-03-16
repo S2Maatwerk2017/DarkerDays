6b567341c499d64588dd21fbf221362947395051
FunkyPaws <springtouw@hotmail.com>
2017-03-16 14:45:15 +0100

    Player movement
    player movement met pijltjes of WASD. zonder animatie.

--- Assets/Script/MovemnetPlayerController.cs
+++ Assets/Script/MovemnetPlayerController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovemnetPlayerController : MonoBehaviour {
+public class MovemnetPlayerController : MonoBehaviour
+{
+    public float MoveSpeed;
 
 	// Use this for initialization
 	void Start () {
@@ -10,7 +12,15 @@ public class MovemnetPlayerController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if (Input.GetAxisRaw( "Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * MoveSpeed * Time.deltaTime, 0f, 0f));
+        }
+        if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * MoveSpeed * Time.deltaTime, 0f));
+        }
 	}
 }
