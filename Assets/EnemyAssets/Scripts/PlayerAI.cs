81cfe3ed305ac58427c4d23ae3c78e01f0516839
Niels Verheijen <kahlan147@gmail.com>
2017-03-23 10:25:53 +0100

    EnemyAI begin, player movement works.

---
+++ Assets/EnemyAssets/Scripts/PlayerAI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAI : MonoBehaviour {
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var HorSpeed = Input.GetAxis("Horizontal") * Time.deltaTime * 5.0f;
+        var VerSpeed = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+        transform.Translate(HorSpeed, -VerSpeed, 0);
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            other.gameObject.GetComponent<AIMovement>().PlayerIsSpotted();
+        }
+    }
+
+}
+
