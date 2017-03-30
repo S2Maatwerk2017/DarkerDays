8c0ddbca0809755faaf02716a62cf0fcc2dec28c
Randy van Poppel <randyvanpoppel@gmail.com>
2017-03-30 11:05:26 +0200

    InitialSave

---
+++ Assets/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float speed;
+    public Player player;
+    private Rigidbody2D myRigidBody2D;
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void OnTriggerEnter2D(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //Player take damage
+            Destroy(gameObject);
+        }
+    }
+}
