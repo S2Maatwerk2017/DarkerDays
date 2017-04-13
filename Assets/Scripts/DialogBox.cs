41a2d04d8316ee76dc9f0af2d20c632b2ead6d60
jethro <jethrovanderkolk@gmail.com>
2017-04-13 14:54:34 +0200

    player stopt met lopen wanneer de dialogbox word weergegeven.

--- Assets/Scripts/DialogBox.cs
+++ Assets/Scripts/DialogBox.cs
@@ -8,13 +8,14 @@ using UnityEngine.UI;
 public class DialogBox : MonoBehaviour
 {
     public GameObject TextBox;
-    public ShopKeeper shopkeeper;
+    public ShopKeeper Currentshopkeeper;
+    public MovemnetPlayerController playermovement;
     public Text textToShow;
 
     public int currentDialog;
     public int currentLine;
     public int endOfLine;
-
+    public bool stopPlayerMovement;
 
     // Use this for initialization
     void Start()
@@ -29,36 +30,38 @@ public class DialogBox : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (shopkeeper.playerCollide)
-        {
-            SetDialogBox(true);
-            shopkeeper.playerCollide = false;
-        }
-        if (Input.GetKeyDown(KeyCode.V))
-        {
-            Debug.Log("+1");
-            currentLine += 1;
-        }
-        if (currentLine >= endOfLine)
-        {
-            Debug.Log(currentLine);
-            Debug.Log("Aantal lines " + shopkeeper.Dialogs[currentDialog].Lines.Count);
-            Debug.Log("AAntal dialog" + shopkeeper.Dialogs.Count);
-            Debug.Log(endOfLine);
-            SetDialogBox(false);
-            currentLine = 0;
-        }
-        else
-        {
-            textToShow.text = shopkeeper.Dialogs[0].Lines[currentLine];
-        }
-        if (shopkeeper != null)
-        {
-            SetEndOfLine();
-        }
+        if (Currentshopkeeper.playerCollide)
+            {
+                SetDialogBox(true);
+                Currentshopkeeper.playerCollide = false;
+            }
+            if (Input.GetKeyDown(KeyCode.V))
+            {
+                Debug.Log("+1");
+                currentLine += 1;
+            }
+            if (currentLine >= endOfLine)
+            {
+                Debug.Log(currentLine);
+                Debug.Log("Aantal lines " + Currentshopkeeper.Dialogs[currentDialog].Lines.Count);
+                Debug.Log("AAntal dialog" + Currentshopkeeper.Dialogs.Count);
+                Debug.Log(endOfLine);
+                SetDialogBox(false);
+                currentLine = 0;
+
+            }
+            else
+            {
+                textToShow.text = Currentshopkeeper.Dialogs[0].Lines[currentLine];
+            }
+            if (Currentshopkeeper != null)
+            {
+                SetEndOfLine(Currentshopkeeper);
+            }
+        
     }
 
-    public void SetEndOfLine()
+    public void SetEndOfLine(ShopKeeper shopkeeper)
     {
         if (endOfLine == 0)
         {
@@ -70,5 +73,13 @@ public class DialogBox : MonoBehaviour
     public void SetDialogBox(bool value)
     {
         TextBox.SetActive(value);
+        if (!value)
+        {
+            playermovement.canMove = true;
+        }
+        else
+        {
+            playermovement.canMove = false;
+        }
     }
 }
