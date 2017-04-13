7d71102b0a674d432e953f8636d4d8f1b86d3b36
= <=>
2017-04-13 12:22:29 +0200

    fmlifeless

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Item 
+{
+
+    public string Name { get; private set; }
+    public int Price { get; private set; }
+    public string Description { get; private set; }
+    public int Amount { get; private set; }
+
+    public Item(string name, int price, string description, int amount)
+    {
+        this.Name = name;
+        this.Price = price;
+        this.Description = description;
+        this.Amount = amount;
+    }
+}
---
+++ Assets/Scripts/Knight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : Enemy, IStrategy {
+
+    private int Timer = 0;
+    public override void DoBehavior()
+    {
+        base.RunToPlayer();
+    }
+
+    public override void Attack()
+    {
+        if (!Player.GetComponent<PlayerHealthManager>().iFramesActive)
+        {
+            Player.GetComponent<PlayerHealthManager>().HurtPlayer(Damage);
+            var clone = (GameObject)Instantiate(DamageNumber, Player.GetComponent<Transform>().position + new Vector3(0f, 2f, 0.5f),
+                    Quaternion.Euler(90f, 0f, 0f));
+            clone.GetComponent<DamageNumbers>().damageNumber = Damage;
+        }
+    }
+}
