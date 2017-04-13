e4ff3d81ac83a887fd0174f2ff95c0f48cb78a29
Randy van Poppel <randyvanpoppel@gmail.com>
2017-04-13 11:30:00 +0200

    NPC's Added

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using Assets.Scripts;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NPC : MonoBehaviour
 {
     public List<Dialog> Dialogs { get; private set; }
+    public Rigidbody MyRigidbody;
+    public NavMeshAgent agent;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
 		Dialogs = new List<Dialog>();
+        MyRigidbody = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/RandomNPC.cs
+++ Assets/Scripts/RandomNPC.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class RandomNPC : MonoBehaviour {
+public class RandomNPC : NPC {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Tooltip("The speed at which the enemy moves")]
+    [Range(0, 7)]
+    private int timer;
+
+    public float MoveSpeed;
+
+    private void Start()
+    {
+
+    }
+    private void Update()
+    {
+        if (timer >= 120)
+        {
+            Move();
+            timer = 0;
+        }
+        timer++;
+    }
+
+        private void Move()
+    {
+        int x = (int)Random.Range(-3, 3);
+        int z = (int)Random.Range(-3, 3);
+
+        Vector3 NextLocation = this.transform.position;
+        NextLocation.x += (x * MoveSpeed);
+        NextLocation.z += (z * MoveSpeed);
+
+        agent.SetDestination(NextLocation);
+    }
 }
