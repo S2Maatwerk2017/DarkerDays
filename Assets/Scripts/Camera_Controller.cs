020ee7f0f61afcf0a3b4a7c7d6e02cf1b68b39e1
Teun van Laarhoven <teunvanlaarhoven@gmail.com>
2017-04-19 11:04:01 +0200

    wip map collision

--- Assets/Scripts/Camera_Controller.cs
+++ Assets/Scripts/Camera_Controller.cs
@@ -19,7 +19,7 @@ public class Camera_Controller : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        targetPosition = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+        targetPosition = new Vector3(followTarget.transform.position.x, transform.position.y, followTarget.transform.position.z);
         transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
 	}
 }
