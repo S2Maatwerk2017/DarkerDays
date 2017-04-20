aa0284d4663803622656468c3b0cb1d62e778ade
Spacegod007 <spacegod007@outlook.com>
2017-04-20 19:09:53 +0200

    Start en Died scherm toegevoegd + script om levels te laden

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class LoadLevel : MonoBehaviour
+{
+	public void loadLevel(string scenename)
+	{
+		Application.LoadLevel(scenename);
+	}
+
+	public void reloadCurrentLevel()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
