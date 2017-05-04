97c7dd32ef0b1cb1005974f877bf0a2c38c95c93
Michelle Broens <michellebroens96@gmail.com>
2017-05-04 10:50:44 +0200

    Skills toegevoegd

---
+++ Assets/Scripts/Skills/Skill.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Skill : MonoBehaviour
+{
+    //properties
+    public string Name { get; private set; }
+    public int Damage { get; private set; }
+    public int Cost { get; private set; }
+    public int CastingTime { get; private set; }
+    public string Description { get; private set; }
+
+    //constructor
+    public Skill(string name, int damage, int cost, int castingtime, string description)
+    {
+        this.Name = name;
+        this.Damage = damage;
+        this.Cost = cost;
+        this.CastingTime = castingtime
+        this.Description = description;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
